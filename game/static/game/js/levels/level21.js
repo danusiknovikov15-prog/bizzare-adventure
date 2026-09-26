6ce88e36752c83c4a889540bb3fe13b00a8990eb
danusiknovikov15-prog <danusiknovikov15@gmail.com>
2026-09-26 20:19:38 +0200

    Add level21.js

---
+++ game/static/game/js/levels/level21.js
@@ -0,0 +1,132 @@
+// Level 21 - Neon Ruins
+export const level21 = {
+    name: "Level 21: Neon Ruins",
+    playerSpawn: { x: 100, y: 500 },
+    backgroundColor: '#10152f',
+    platforms: [
+    {
+        "x": 0,
+        "y": 550,
+        "width": 500,
+        "height": 50,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 580,
+        "y": 490,
+        "width": 120,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 760,
+        "y": 430,
+        "width": 120,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 940,
+        "y": 500,
+        "width": 150,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 1160,
+        "y": 450,
+        "width": 130,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 1360,
+        "y": 380,
+        "width": 130,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 1560,
+        "y": 460,
+        "width": 180,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 1810,
+        "y": 390,
+        "width": 140,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 2010,
+        "y": 320,
+        "width": 160,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 2240,
+        "y": 400,
+        "width": 180,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 2480,
+        "y": 300,
+        "width": 180,
+        "height": 18,
+        "color": "#00e5ff"
+    },
+    {
+        "x": 2700,
+        "y": 220,
+        "width": 260,
+        "height": 40,
+        "color": "#ffffff"
+    }
+],
+    bounds: { minX: 0, maxX: 3100, minY: 0, maxY: 600 },
+    door: { x: 2800, y: 160 },
+    goal: { x: 2700, y: 160, width: 260, height: 60 },
+    enemies: [
+    {
+        "x": 650,
+        "y": 430,
+        "isElite": false
+    },
+    {
+        "x": 980,
+        "y": 440,
+        "isElite": false
+    },
+    {
+        "x": 1200,
+        "y": 390,
+        "isElite": true
+    },
+    {
+        "x": 1580,
+        "y": 400,
+        "isElite": false
+    },
+    {
+        "x": 1850,
+        "y": 330,
+        "isElite": true
+    },
+    {
+        "x": 2280,
+        "y": 340,
+        "isElite": false
+    },
+    {
+        "x": 2520,
+        "y": 240,
+        "isElite": true
+    }
+]
+};
